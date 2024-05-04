a20f0585024b04ee9ce34c21c43b5f61a32cf16d
NixonRus <167446421+NixonRus@users.noreply.github.com>
2024-05-04 19:57:35 +0700

    Add files via upload

---
+++ HomeWork4.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 'set', 3, 'door', 7, False
+print(immutable_var)
+#immutable_var [1] = 'cat' #нельзя изменять объекты кортежа, за исключением списков внутри кортежа
+
+mutable_list = [5, 'dog', 8, True, 4, 'mouse']
+print(mutable_list)
+mutable_list [1] = 'cat'
+print(mutable_list)
